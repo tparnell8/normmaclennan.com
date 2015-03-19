b31f192b9148fe45830b34136e28e4792188a846
Norm MacLennan <norm.maclennan@gmail.com>
2015-03-18 22:27:25 -0400

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function makeUL(array) {
+  var list = document.createElement('ul');
+
+  array.forEach(function(e){
+    var item = document.createElement('li');
+    var link = document.createElement('a');
+    link.href = e.link;
+    link.appendChild(document.createTextNode(e.title));
+    item.appendChild(link);
+
+    list.appendChild(item);
+  });
+
+  return list;
+}
+
+jQuery.getFeed({
+  url: 'https://blog.normmaclennan.com/rss/',
+  success: function(feed) {
+    document.getElementById('blog-items').appendChild(makeUL(feed.items));
+    var placeholder = document.getElementById('blog-placeholder');
+    placeholder.parentNode.removeChild(placeholder);
+  }
+});
